e16340a5cbc247cb3fd420d2a515f5e077ceb882
alecBeccaria <80131757+alecBeccaria@users.noreply.github.com>
2021-10-20 20:28:05 -0600

    updates

--- WikiParser.py
+++ WikiParser.py
@@ -3,43 +3,53 @@ from io import StringIO
 from collections import defaultdict
 import lxml.etree as etree
 import pymongo as db
+from pymongo import MongoClient
+import re
 import codecs
-import pandas as csv
+import json
 
-regex = "/Category:([a-zA-Z]+) \(constellation\)/gm"
+regex = "\[\[.*\(constellation\)\]\]"
 
 def xml_parser(xmlFile):
-    with open('text.txt', 'w', encoding='utf-8') as file:
-        print('Starting Parse')
-        xml_root = etree.iterparse(xmlFile, events=('start', 'end'))
+    wikiDic = {}
 
-        nsOne = 'http://www.mediawiki.org/xml/export-0.10/'
-        ns = {'x': 'http://www.mediawiki.org/xml/export-0.10/'}
-        importCusCount = 0
-        elementList = []
-        for ev, elem in xml_root:
-            tag = get_tag_name(elem.tag, elem)
-            if ev == 'start':
-                if tag == 'page':
-                    title = ''
-                    id = -1
-                    redirect = ''
-                    inrevison = False
-                    ns = 0
-                elif tag == 'text':
-                    text = elem.text
-                    if text != None:
-                        file.write(text)
-                elif tag == 'id' and not inrevison and elem.text != None:
-                    id = int(elem.text)
-                elif tag == 'redirect':
-                    redirect = elem.get('title', '')
-                elif tag == 'ns' and elem.text != None:
-                    ns = int(elem.text)
-            elif tag == 'page':
-                print()
-            elem.clear()
+    print('Starting Parse')
+    xml_root = etree.iterparse(xmlFile, events=('start', 'end'))
+    print('Parse Done')
+
+
+    nsOne = 'http://www.mediawiki.org/xml/export-0.10/'
+    ns = {'x': 'http://www.mediawiki.org/xml/export-0.10/'}
+    importCusCount = 0
+    elementList = []
+    for ev, elem in xml_root:
+        tag = get_tag_name(elem.tag, elem)
+        if ev == 'start':
+            if tag == 'page':
+                title = ''
+                id = -1
+                redirect = ''
+                inrevison = False
+                ns = 0
+            elif tag =='title':
+                wikiDic['title'] = elem.text
+            elif tag == 'text':
+                if elem.text != None:
+                    match = re.search(regex, elem.text)
+                    if match != None:
+                        wikiDic['text'] = elem.text
+            elif tag == 'id' and not inrevison and elem.text != None:
+                id = int(elem.text)
+            elif tag == 'redirect':
+                redirect = elem.get('title', '')
+            elif tag == 'ns' and elem.text != None:
+                ns = int(elem.text)
+        elem.clear()
+    with open('data.txt', 'w') as file:
+        json.dump(wikiDic, file)
         file.close()
+        wikiDic.clear()
+
 
 
 def get_tag_name(tag, elem):
@@ -51,15 +61,21 @@ def get_tag_name(tag, elem):
 
 
 def testConn():
-    connectionString = 'Driver={SQL Server};Server=tcp:sadserver1.database.windows.net,1433;Database=dbNW;Uid=sad666;Pwd=Ophelia?;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
-    connection = db.connect(connectionString)
-    cursor = connection.cursor()
-    connStr = cursor.execute('SELECT 1')
-    print(connStr)
+    connectionString = 'mongodb+srv://root:tortilla@cluster0.mpjye.mongodb.net/Wookie?retryWrites=true&w=majority'
+    client = MongoClient(connectionString)
+    server = client._get_server_session()
+    database = client.get_database('Wookie')
+    collection = database.get_collection('WikiShiz')
+
+
+
+
+
 
 
 if __name__ == "__main__":
     # testConn()
     # xmlFile = open('customers.xml', 'r')
-    xml_parser('wiki.xml-p41496246p42996245')
+    testConn()
+    #xml_parser('enwiki-latest-pages-articles22.xml-p41496246p42996245')
     # sql_dbms(customers)
