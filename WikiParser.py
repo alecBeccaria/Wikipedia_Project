7a6d9844dd561095836e536b920b63cc3ed7cef0
alecBeccaria <80131757+alecBeccaria@users.noreply.github.com>
2021-10-25 20:19:59 -0600

    Update WikiParser.py

--- WikiParser.py
+++ WikiParser.py
@@ -6,12 +6,17 @@ import pymongo as db
 from pymongo import MongoClient
 import re
 import codecs
+import certifi
 import json
 
-regex = "\[\[.*\(constellation\)\]\]"
+constRegex = "\[\[.*\(constellation\)\]\]"
+starboxRegex = "^\{\{Starbox begin(.|\n)*?\}\}((.|\n)*?)\{\{Starbox end\}\}"
+constellRegex = "(constell = )\[\[(\w*)"
+
 
 def xml_parser(xmlFile):
-    wikiDic = {}
+    wikiList = []
+
 
     print('Starting Parse')
     xml_root = etree.iterparse(xmlFile, events=('start', 'end'))
@@ -23,6 +28,7 @@ def xml_parser(xmlFile):
     importCusCount = 0
     elementList = []
     for ev, elem in xml_root:
+        starDic = {}
         tag = get_tag_name(elem.tag, elem)
         if ev == 'start':
             if tag == 'page':
@@ -31,25 +37,28 @@ def xml_parser(xmlFile):
                 redirect = ''
                 inrevison = False
                 ns = 0
-            elif tag =='title':
-                wikiDic['title'] = elem.text
-            elif tag == 'text':
+            if tag == 'title':
+                title = elem.text
+            if tag == 'text':
                 if elem.text != None:
-                    match = re.search(regex, elem.text)
+                    match = re.search(starboxRegex, elem.text)
                     if match != None:
-                        wikiDic['text'] = elem.text
-            elif tag == 'id' and not inrevison and elem.text != None:
-                id = int(elem.text)
-            elif tag == 'redirect':
-                redirect = elem.get('title', '')
-            elif tag == 'ns' and elem.text != None:
-                ns = int(elem.text)
-        elem.clear()
-    with open('data.txt', 'w') as file:
-        json.dump(wikiDic, file)
-        file.close()
-        wikiDic.clear()
+                        starDic['title'] = title
+                        print(starDic)
+                        # constell = re.search(constellRegex, elem.text)
+                        # if constell != None:
+                        #     starDic['title'] = title
+                        #     starDic['constellation'] = constell.group(2)
+                        #     wikiList.append(starDic)
+                        #     print(starDic)
 
+
+
+        elem.clear()
+    with open('data.json', 'w') as file:
+        json.dump(wikiList, file)
+        file.close()
+        print(wikiList)
 
 
 def get_tag_name(tag, elem):
@@ -60,12 +69,16 @@ def get_tag_name(tag, elem):
     return t
 
 
-def testConn():
-    connectionString = 'mongodb+srv://root:tortilla@cluster0.mpjye.mongodb.net/Wookie?retryWrites=true&w=majority'
-    client = MongoClient(connectionString)
+def connDB():
+    ca = certifi.where()
+    connectionString = 'mongodb+srv://user:bob@cluster0.mpjye.mongodb.net/?retryWrites=true&w=majority'
+    client = db.MongoClient(connectionString, tlsCAFILE=ca)
+    print(client.server_info())
+    connection = client['Wookie']
     server = client._get_server_session()
     database = client.get_database('Wookie')
-    collection = database.get_collection('WikiShiz')
+    collection = connection['WikiShiz']
+
 
 
 
@@ -76,6 +89,6 @@ def testConn():
 if __name__ == "__main__":
     # testConn()
     # xmlFile = open('customers.xml', 'r')
-    testConn()
-    #xml_parser('enwiki-latest-pages-articles22.xml-p41496246p42996245')
+    connDB()
+    xml_parser('enwiki-latest-pages-articles22.xml-p41496246p42996245')
     # sql_dbms(customers)
